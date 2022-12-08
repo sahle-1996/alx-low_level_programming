b228416e526249191fc1cbe9c9f32abf5f1afac1
sahle-1996 <sahlesiumgoitom@gmail.com>
2022-12-08 12:06:41 -0800

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -11,13 +9,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzz");
+	return (0);
 }
