fc8a3a8dabc7c7e61cdcdf16aa66d6eb9b879ea4
sahle-1996 <sahlesiumgoitom@gmail.com>
2022-12-08 12:37:32 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,11 @@
  * Description: 'Printing with out printf'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzz");
 	return (0);
+
+
 }
