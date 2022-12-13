44fe9152e9ae16f450dea3392962fac4c26205da
sahle-1996 <sahlesiumgoitom@gmail.com>
2022-12-13 14:38:26 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,23 +10,16 @@
 void times_table(void)
 
 {
-
 	int i, j, n;
 
 	for (i = 0; i <= 9; i++)
-
 	{
-
 		for (j = 0; j <= 9; j++)
-
 		{
-
 			n = i * j;
-
 			if ((n / 10) == 0)
 
 			{
-
 				if (j != 0)
 
 					_putchar(' ');
@@ -36,35 +29,23 @@ void times_table(void)
 				if (j == 9)
 
 					continue;
-
 				_putchar(',');
 
 				_putchar(' ');
-
 			}
-
 			else
-
 			{
-
 				_putchar((n / 10) + '0');
 
 				_putchar((n % 10) + '0');
-
 				if (j == 9)
-
 					continue;
-
 				_putchar(',');
 
 				_putchar(' ');
-
 			}
-
 		}
-
 		_putchar('\n');
-
 	}
 
 }
