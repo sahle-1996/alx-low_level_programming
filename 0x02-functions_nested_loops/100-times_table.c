dd0c413be2d53750ba82261041d1c82b1728e405
sahle-1996 <sahlesiumgoitom@gmail.com>
2022-12-14 12:26:14 -0800

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
  */
 
 void print_times_table(int n)
-
 {
 
 	int num, mult, product;
@@ -19,46 +18,31 @@ void print_times_table(int n)
 
 		{
 			_putchar('0');
-
 			for (mult = 1; mult <= n; mult++)
-
 			{
 				_putchar(',');
-
 				_putchar(' ');
 
 				product = num * mult;
-
 				if (product <= 99)
 
 					_putchar(' ');
-
 				if (product <= 9)
-
 					_putchar(' ');
-
 				if (product >= 100)
-
 				{
 					_putchar((product / 100) + '0');
-
 					_putchar(((product / 10)) % 10 + '0');
-
 				}
 
 				else if (product <= 99 && product >= 10)
-
 				{
-
 					_putchar((product / 10) + '0');
-
 				}
 				_putchar((product % 10) + '0');
 			}
-
 			_putchar('\n');
 		}
-
 	}
 
 }
