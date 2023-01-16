a40e0336349cdfd259c64775c37c28e11eeac3a7
sahle-1996 <sahlesiumgoitom@gmail.com>
2023-01-16 10:25:43 -0800

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Eromosele Eric
+ */
+
 #include <stdio.h>
+
 /**
-* main - prints the name of the file program was compiled from.
-* Return: Always 0
-*/
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: 0 for success.
+ */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
