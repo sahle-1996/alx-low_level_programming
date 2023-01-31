5fdaefdf268bfaa624cf029bcc75bc8d4ed95427
sahle-1996 <sahlesiumgoitom@gmail.com>
2023-01-31 07:21:14 -0800

    get_nodeint at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int h;
 
-	for (i = 0; h < index && head != NULL; h++)
+	for (h = 0; h < index && head != NULL; h++)
 	{
 		head = head->next;
 	}
