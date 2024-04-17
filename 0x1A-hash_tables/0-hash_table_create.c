4906ef096e7412101c1d61396195879f2f50694c
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-04-17 01:01:40 -0700

    o-hash_table_create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: is the size of the array
+ *
+ * Return: a pointer to the newly created hash table or NULL
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table;
+
+	if (size == 0)
+		return (NULL);
+
+	h_table = malloc(sizeof(hash_table_t));
+	if (!h_table)
+		return (NULL);
+
+	h_table->size = size;
+	h_table->array = calloc((size_t)h_table->size, sizeof(hash_node_t *));
+	if (h_table->array == NULL)
+	{
+		free(h_table);
+		return (NULL);
+	}
+
+	return (h_table);
+}
